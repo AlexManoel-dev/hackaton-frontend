2f265c19b58443084ca61b89d37a5ea12c05cd1e
Alex Manoel <alex.manoel@gestaokaizen.com.br>
2024-12-06 01:15:48 -0300

    feature(project): Websocket implemented

--- src/app/heat-exchanger/heat-exchanger.component.ts
+++ src/app/heat-exchanger/heat-exchanger.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { WebsocketService } from '../data/services/websocket.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-heat-exchanger',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './heat-exchanger.component.html',
   styleUrl: './heat-exchanger.component.scss'
 })
-export class HeatExchangerComponent {
+export class HeatExchangerComponent implements OnInit {
+  sensorData: any;
 
+  constructor(private websocketService: WebsocketService) {}
+
+  ngOnInit(): void {
+    // Escuta o evento 'sensor-data'
+    this.websocketService.listen('sensor-data').subscribe((data) => {
+      this.sensorData = data;
+    });
+  }
 }
